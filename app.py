99bd691ef1fb6b35b453a3b35af21bb4feb0504b
Arik Rizki Akbar <arz147@upi.edu>
2023-06-02 14:40:05 +0700

    modify flask api structur, connecting with swagger

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-from flask import Flask, request, jsonify
-from predictSymptom import predictSymptom
+from flask import Flask, request
+import predictSymptom
+from flask_restx import Api, Resource
 
 # Initialize Flask
 app = Flask(__name__)
+api = Api(app, title='Diagnese API', description='This is Diagnese API Documentation')
+
+ns = api.namespace('diagnese', description='Namespace description')
+
 
 # Initialize Flask server (file prediction.py)
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET"])
 def welcome():
     return "Welcome to our API"
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.get_json()
+@ns.route('/predict')
+class Prediction(Resource):
+    @ns.doc(description='diagnose by symptom')
+    def post(self):
+        data = request.get_json()
 
-    return predictSymptom(data)
+        return predictSymptom.predictSymptom(data)
+    def get(self):
+        
+        return predictSymptom.getAllDignosis()
 
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True) #uncomment if you want to run on GCP
     app.run(port=8080, debug=True) #uncomment if you want to run on local
+
+
+@api.route('/swagger')
+class Swagger(Resource):
+    def get(self):
+        return api.__schema__
